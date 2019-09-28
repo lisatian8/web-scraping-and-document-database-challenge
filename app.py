f11aa315f59222f89e7dec35d65885cbcf86524d
lisatian8 <lt8760@yahoo.com>
2019-09-28 07:00:26 -0400

    app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-@app.route("/")
-def index():
-    hurricanes = list(db.collection.find())
-    print(hurricanes)
-    return render_template("index.html", hurricanes=hurricanes)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
